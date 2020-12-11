1d69ec5df5c498e11adb0c71d916ec21196e03ac
si4141 <ced4141@me.com>
2020-12-11 17:55:22 +0900

    add meta tag

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,13 +5,20 @@ import SiteHeader from './siteHeader'
 export default function Layout(props) {
   const {title, children} = props;
   const siteTitle = 'ヨンフィクション Official Web Site'
+  const titleText = title ? `${title} | ${siteTitle}` : siteTitle;
 
   return (
     <>
       <Head>
-        <title>{title ? `${title} | ${siteTitle}` : siteTitle}</title>
+        <title>{titleText}</title>
+        <meta content={titleText} name="title" />
         <meta name="description" content="ロックバンド ヨンフィクションの公式webサイトです。"></meta>
         <script src="https://kit.fontawesome.com/69cad91b52.js" crossOrigin="anonymous"></script>
+        <meta property="og:type" content={title ? "website": "article"}/>
+        <meta property="og:title" content={titleText} />
+        <meta property="og:url" content="https://4-fiction.tokyo" />
+        <meta property="og:site_name" content={siteTitle} />
+        <meta property="og:locale" content="ja_JP" />
       </Head>
       <div className={styles.container}>
         <SiteHeader />
